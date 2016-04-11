e966463fb369d06134e5e99b22fd97caa24e3022
marklarr <mark.larsen@venmo.com>
2016-04-10 21:41:39 -0700

    GameBoardService can create and read boards

---
+++ web/services/game_board_service.ex
@@ -0,0 +1,35 @@
+defmodule DotsServer.GameBoardService do
+  alias DotsServer.GameBoard
+
+  def create_board(size) do
+    attrs = %{board_lines: encode(init_board_lines(size)), board_fills: encode(init_board_fills(size))}
+    changeset = GameBoard.changeset(%GameBoard{}, attrs)
+    DotsServer.Repo.insert!(changeset)
+  end
+
+  def board_lines(game_board) do
+    decode(game_board.board_lines)
+  end
+
+  def board_fills(game_board) do
+    decode(game_board.board_fills)
+  end
+
+  defp decode(board_array) do
+    Poison.decode!(board_array)
+  end
+
+  defp encode(board_array) do
+    Poison.encode!(board_array)
+  end
+
+  defp init_board_lines(size) do
+    Enum.map 1..size, fn _x ->
+      Enum.map 1..size, fn _x -> 0 end
+    end
+  end
+
+  defp init_board_fills(size) do
+    init_board_lines(size - 1)
+  end
+end
