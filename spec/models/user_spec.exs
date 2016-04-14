b44dd455f1083192d197567532ffd49729f4e0b5
marklarr <mark.larsen@venmo.com>
2016-04-13 19:38:51 -0700

    add addict dependenct and create a User model

---
+++ spec/models/user_spec.exs
@@ -0,0 +1,25 @@
+defmodule DotsServer.UserSpec do
+  use ESpec
+
+  alias DotsServer.User
+
+  describe "#changeset" do
+    context "valid attributes" do
+      let :valid_attrs, do: %{email: "some content", encrypted_password: "some content", handle: "some content"}
+
+      it "makes a valid changeset" do
+        changeset = User.changeset(%User{}, valid_attrs)
+        changeset.valid? |> should(be_true)
+      end
+    end
+  end
+
+  context "changeset with invalid attributes" do
+    let :invalid_attrs, do: %{}
+
+    it "makes an invalid changeset" do
+      changeset = User.changeset(%User{}, invalid_attrs)
+      changeset.valid? |> should(be_false)
+    end
+  end
+end
