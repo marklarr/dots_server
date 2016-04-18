0b18767f708e913969ba4dd1f59d5d894895f9b1
marklarr <mark.larsen@venmo.com>
2016-04-17 17:45:16 -0700

    GameBoards know the winner and if they are complete or not

--- spec/models/game_board_spec.exs
+++ spec/models/game_board_spec.exs
@@ -2,6 +2,63 @@ defmodule DotsServer.GameBoardSpec do
   use ESpec
 
   alias DotsServer.GameBoard
+  alias DotsServer.BoardLines
+  alias DotsServer.BoardFills
+  alias DotsServer.GameBoardUser
+
+  import DotsServer.Factory
+
+  let :user1, do: create(:user)
+  let :user2, do: create(:user)
+
+  defp attach_user(game_board, user) do
+    DotsServer.Repo.insert!(%GameBoardUser{game_board_id: game_board.id, user_id: user.id})
+    game_board
+  end
+
+  let :tied_game_over_game_board do
+      board_lines = [
+        [user2.id, user2.id],
+        [user1.id, user1.id, user1.id],
+        [user2.id, user1.id],
+        [user1.id, user2.id, user1.id],
+        [user2.id, user2.id]
+      ]
+
+      board_fills = [
+        [user2.id, user1.id],
+        [user2.id, user1.id]
+      ]
+
+      create(:game_board, %{board_lines_data: BoardLines.data(board_lines), board_fills_data: BoardFills.data(board_fills)})
+      |> attach_user(user1)
+      |> attach_user(user2)
+  end
+
+  let :user2_wins_game_over_game_board do
+      board_lines = [
+        [user2.id, user2.id],
+        [user1.id, user1.id, user1.id],
+        [user2.id, user1.id],
+        [user1.id, user2.id, user1.id],
+        [user2.id, user2.id]
+      ]
+
+      board_fills = [
+        [user2.id, user1.id],
+        [user2.id, user2.id]
+      ]
+
+      create(:game_board, %{board_lines_data: BoardLines.data(board_lines), board_fills_data: BoardFills.data(board_fills)})
+      |> attach_user(user1)
+      |> attach_user(user2)
+  end
+
+  let :new_game_board do
+    create(:game_board)
+    |> attach_user(user1)
+    |> attach_user(user2)
+  end
 
   describe "#changeset" do
     context "valid attributes" do
@@ -19,6 +76,48 @@ defmodule DotsServer.GameBoardSpec do
       it "makes an invalid changeset" do
         changeset = GameBoard.changeset(%GameBoard{}, invalid_attrs)
         changeset.valid? |> should(be_false)
+      end
+    end
+  end
+
+  describe "game_over?(game_board)" do
+    it "is true if all board_lines are filled" do
+      tied_game_over_game_board
+      |> GameBoard.game_over?
+      |> should(eq true)
+
+      user2_wins_game_over_game_board
+      |> GameBoard.game_over?
+      |> should(eq true)
+    end
+
+    it "is false if there are remaining board_lines" do
+      new_game_board
+      |> GameBoard.game_over?
+      |> should(eq false)
+    end
+  end
+
+  describe "winner_user(game_board)" do
+    context "game is not over yet" do
+      it "there is no winner yet" do
+        new_game_board
+        |> GameBoard.winner_user
+        |> should(eq nil)
+      end
+    end
+
+    context "game is over" do
+      it "is the user with more board_fills" do
+        user2_wins_game_over_game_board
+        |> GameBoard.winner_user
+        |> should(eq user2)
+      end
+
+      it "is nil if users are tied for board_fills" do
+        tied_game_over_game_board
+        |> GameBoard.winner_user
+        |> should(eq nil)
       end
     end
   end
--- web/models/game_board.ex
+++ web/models/game_board.ex
@@ -1,7 +1,8 @@
 defmodule DotsServer.GameBoard do
   use DotsServer.Web, :model
 
-  # @board_regex ~r/^[\[\],01]+$/
+  alias DotsServer.BoardLines
+  alias DotsServer.BoardFills
 
   schema "game_boards" do
     field :board_lines_data, :binary
@@ -25,7 +26,38 @@ defmodule DotsServer.GameBoard do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
-    # |> validate_format(:board_fills_data, @board_regex)
-    # |> validate_format(:board_lines_data, @board_regex)
+  end
+
+  def winner_user(game_board) do
+    if game_over?(game_board) do
+      fills_by_user = game_board.board_fills_data
+      |> BoardFills.parse
+      |> List.flatten
+      |> Enum.group_by(fn(user_id) -> user_id end)
+
+      game_board = DotsServer.Repo.preload(game_board, :users)
+      user1 = Enum.at(game_board.users, 0)
+      user2 = Enum.at(game_board.users, 1)
+      user1_score = fills_by_user
+                    |> Map.get(user1.id)
+                    |> Enum.count
+      user2_score = fills_by_user
+                    |> Map.get(user2.id)
+                    |> Enum.count
+      cond do
+        user1_score > user2_score -> user1
+        user2_score > user1_score -> user2
+        user1_score == user2_score -> nil
+      end
+    else
+      nil
+    end
+  end
+
+  def game_over?(game_board) do
+    !(game_board.board_lines_data
+    |> BoardLines.parse
+    |> List.flatten
+    |> Enum.member?(nil))
   end
 end
