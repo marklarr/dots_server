61b7b3901772e2c4a21832b6006d8d543677eeae
marklarr <mark.larsen@venmo.com>
2016-04-24 13:50:03 -0700

    wip/GameBoardChannel - added some failing tests for handling errors

--- spec/channels/game_board_channel_spec.exs
+++ spec/channels/game_board_channel_spec.exs
@@ -65,44 +65,63 @@ defmodule DotsServer.GameBoardChannelTest do
   end
 
   describe "take_turn" do
-    it "takes the turn and broadcasts the new game_board to all sockets" do
-      expected_game_board = %{
-        "game_board" => %{
-          "id" => game_board.id,
-          "board_lines" => [
-            [nil, nil],
-            [nil, nil, nil],
-            [nil, nil],
-            [nil, user1.id, nil],
-            [nil, nil]
-          ],
-          "board_fills" => [
-            [nil, nil],
-            [nil, nil]
-          ],
-          "users" => [
-            %{
-              "id" => user1.id,
-              "handle" => user1.handle,
-              "email" => user1.email
-            },
-            %{
+    context "valid move" do
+      it "takes the turn and broadcasts the new game_board to all sockets" do
+        expected_game_board = %{
+          "game_board" => %{
+            "id" => game_board.id,
+            "board_lines" => [
+              [nil, nil],
+              [nil, nil, nil],
+              [nil, nil],
+              [nil, user1.id, nil],
+              [nil, nil]
+            ],
+            "board_fills" => [
+              [nil, nil],
+              [nil, nil]
+            ],
+            "users" => [
+              %{
+                "id" => user1.id,
+                "handle" => user1.handle,
+                "email" => user1.email
+              },
+              %{
+                "id" => user2.id,
+                "handle" => user2.handle,
+                "email" => user2.email
+              }
+            ],
+            "next_turn_user" => %{
               "id" => user2.id,
               "handle" => user2.handle,
               "email" => user2.email
             }
-          ],
-          "next_turn_user" => %{
-            "id" => user2.id,
-            "handle" => user2.handle,
-            "email" => user2.email
           }
         }
-      }
 
-      push socket1, "take_turn", %{from: %{x: 1, y: 1 }, to: %{x: 1, y: 2} }
-      assert_broadcast "updated_game_board", actual_game_board
-      actual_game_board |> should(eq expected_game_board)
+        push socket1, "take_turn", %{from: %{x: 1, y: 1 }, to: %{x: 1, y: 2} }
+        assert_broadcast "updated_game_board", actual_game_board
+        actual_game_board |> should(eq expected_game_board)
+      end
+
+      it "replies with status ok" do
+        ref = push socket1, "take_turn", %{from: %{x: 1, y: 1 }, to: %{x: 1, y: 2} }
+        assert_reply(ref, :ok, _)
+      end
+    end
+
+    context "invalid move" do
+      it "does not broadcast anything to the sockets" do
+        push socket1, "take_turn", %{from: %{x: 1, y: 1 }, to: %{x: 2, y: 2} }
+        refute_broadcast "updated_game_board", _
+      end
+
+      it "replies with a status of error" do
+        ref = push socket1, "take_turn", %{from: %{x: 1, y: 1 }, to: %{x: 2, y: 2} }
+        assert_reply(ref, :error, _)
+      end
     end
   end
 
